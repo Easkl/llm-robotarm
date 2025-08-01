d3b7a17fdafa114e1866b1e39d8833995829b616
easkl <easkl@example.com>
2025-08-01 15:09:17 +0900

    알고리즘 테스트

--- index.py
+++ index.py
@@ -1,23 +1,10 @@
 from google import genai
 import os
-import serial
-import time
 
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
-ser = serial.Serial(
-    port='COM3',  # 장치관리자에서 확인해서 포트 번호는 수정할 것.
-    baudrate=115200,
-    timeout=1
+response = client.models.generate_content(
+    model="gemini-2.5-flash-lite", 
+    contents="why is the sky blue?"
 )
 
-while True:
-    user_input = input("Enter your command: ")
-    if user_input.lower() == "exit":
-        break
-    response = client.models.generate_content(
-        model="gemini-2.5-flash-lite",
-        contents=user_input
-    )
-    print(f"You entered: {user_input}")
-    print(response.candidates[0].content.parts[0].text)
-    ser.write(response.candidates[0].content.parts[0].text.encode('utf-8'))
+print(response.text)
