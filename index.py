245fdf5f529e7785f9faac18ef0989fd52b203f4
easkl <easkl@example.com>
2025-08-01 10:12:08 +0900

    LLM과 통신한 결과를 시리얼 통신으로 esp32에다 전송하는 코드 작성

--- index.py
+++ index.py
@@ -1,7 +1,14 @@
 from google import genai
 import os
+import serial
+import time
 
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
+ser = serial.Serial(
+    port='COM3',  # 장치관리자에서 확인해서 포트 번호는 수정할 것.
+    baudrate=115200,
+    timeout=1
+)
 
 while True:
     user_input = input("Enter your command: ")
@@ -13,3 +20,4 @@ while True:
     )
     print(f"You entered: {user_input}")
     print(response.candidates[0].content.parts[0].text)
+    ser.write(response.candidates[0].content.parts[0].text.encode('utf-8'))
